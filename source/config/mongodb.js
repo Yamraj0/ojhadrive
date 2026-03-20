43d6c865a96e3cd7a5d342f66728642ecf5d13ea
Yamraj0 <ojhayamraj@gmail.com>
2026-03-20 17:51:19 +0545

    setup for final version

--- source/config/mongodb.js
+++ source/config/mongodb.js
@@ -5,10 +5,8 @@ const connectDB =  async () => {
         const response = await mongoose.connect(process.env.MONGODB_URI)
         console.log('Database is connected sucessfully')
     } catch (error) {
-        res.status(500).json({
-            message: "INTERNAL SERVER ERROR WHILE CONNECTING TO THE DATABASE",
-            error : JSON.stringify(error)
-        })
+        console.log("INTERNAL SERVER ERROR WHILE CONNECTING TO THE DATABASE");
+        
     }
 }
 
