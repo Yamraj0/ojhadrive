68cad8b22f3f75c46e184ad3579cd56939aa9f17
Yamraj0 <ojhayamraj@gmail.com>
2026-03-23 12:17:19 +0545

    change telegram setp code

--- source/config/telegram.js
+++ source/config/telegram.js
@@ -3,7 +3,7 @@ import { StringSession } from "telegram/sessions/index.js";
 
 const apiId = 32578171; // your API ID
 const apiHash = "704b71ef30ad3fa125c382439b1322f6"; // your API Hash
-const session = "1BQANOTEuMTA4LjU2LjExMwG7w/XTgzuXUz+fN9TxRaSGifoiu3HsMUu5jCcO0waXYlRFtA+aGt3/GTjwrYb7WPzTDRMCIIsnC3nBJQZOdYM7QluuJY+03sNK6PClEQbM+713xxn90a19lGMC4ai0Vzd5NA8U2KCXJyCuXXDv8o2cDd5ZKOYgRhQ+OCzIQWoE5jAxZNDCxoDi82x3wtcpjUyNTZwlgiKv2pWOsIQ0P0rjsfijQ933YnPS7a5iiKOdfHg0eqNXTeqCD5ebOzs1gBh4bgZMuJvSnMp9RHchOhseQ+e9dqXw5Yzker934KAcUdS9HfT5+ULR1xwXZo5e3Z3h9UACD1jRACn5mUTOp6jjRg=="; // your session string, you can get it by running the TelegramClient and saving the session after logging in   
+const session = "1BVtsOGUBu10R5oTMLUeI1RGJ0UebKEbo-35xd7baNd8dFMY0KHYISdG8tB6LFo8mBZT75kE1WugqTPWgh1MRTP10AlPTHixCUOQZpEHCBFFZb4Vx0gOP_6EqaB1rCQN03YBtn1G6cuMLI3FKqj4zuVEuzDIcVxpsXzujoNqDGq862Rrts2VQL3fqdiFgjUbAn76EFnflonA4h5oAkUOJXxRDNd3Acw5mvf1zWjaCS4HsxaBm4SewuuLHzY3jpMRf7FmkAU4VRsfb6ITpDCGgKfKjEq_pK3G5Qe41HDJqHK6vlP8USRRT1oDab1CWU23cPUXN8WCbLk86g8pC6QFc6vA6h3FRjVg=";
 
 
 const client = new TelegramClient(
